fb89b0ea49a9a8b9f855ec4f0165fc76507f6dfd
Razo <javierrazzzo@hotmail.com>
2019-02-25 17:47:37 -0800

    Initial commit

---
+++ src/main/java/com/uabc/edu/mx/_1149135curriculumvitae/Application.java
@@ -0,0 +1,13 @@
+package com.uabc.edu.mx._1149135curriculumvitae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
